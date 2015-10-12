8cf5222dbf301ee1a5bf22b06332f199d37bd9ec
hs0x01 <kumamon0x01@gmail.com>
2015-10-12 17:42:43 +0900

    Gulp調査。

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 // モジュールの読み込み
-var gulp   = require('gulp');
-var gulpTs = require('gulp-typescript');
-var merge  = require('merge2');
-var watch  = require("gulp-watch");
+var gulp       = require('gulp');
+var gulpTs     = require('gulp-typescript');
+var merge      = require('merge2');
+var watch      = require("gulp-watch");
+var gulpLess   = require('gulp-less');
+var del        = require('del');
+var plumber    = require('gulp-plumber');
 
 // 定数定義
-var TS_TARGET      = 'ES5';
-var JS_OUTPUT_PATH = 'after/js/';
-var CORE_D_TS      = 'core.d.ts';
-var CORE_JS        = 'core.js';
-var FEATURE_JS     = 'feature.js';
+var TS_TARGET       = 'ES5';
+var JS_OUTPUT_PATH  = 'after/js/';
+var CSS_OUTPUT_PATH = 'after/css';
+var CORE_D_TS       = 'core.d.ts';
+var CORE_JS         = 'core.js';
+var FEATURE_JS      = 'feature.js';
+var LESS_FILES      = 'after/less/**/*.less';
 
 var CORE_TS_FILES = ['after/Scripts/extensions/*.ts',
 					 'after/Scripts/lib/*.ts',
@@ -55,18 +60,29 @@ gulp.task('compileAllTs', function () {
 	compileCoreTs();
 });
 
+// すべてのLESSのコンパイル
+gulp.task('compileAllLess', compileAllLess);
+
 // ファイルの追加・更新・削除を監視し、変更があったら再コンパイル実行
 gulp.task('watch', function() {
 
-    watch(CORE_TS_FILES,    compileCoreTs);
-    watch(FEATURE_TS_FILES, compileFeatureTs);
+	// core.jsのためのTypeScriptコンパイル
+    watch(CORE_TS_FILES, compileCoreTs);
     
+    // feature.jsのためのTypeScriptコンパイル
+    watch(FEATURE_TS_FILES, compileFeatureTs)
+    
+    // LESSのコンパイル
+	watch(LESS_FILES, compileAllLess);
+    
+    // core.js更新通知
     watch(JS_OUTPUT_PATH + CORE_JS, function() {
     	console.log('Update core.js');
     });
+    // feature.js更新通知
     watch(JS_OUTPUT_PATH + FEATURE_JS, function() {
     	console.log('Update feature.js');
-    });
+    })
 });
 
 // Coreに属するTypeScriptをコンパイルする関数
@@ -89,3 +105,20 @@ function compileFeatureTs() {
 				.pipe(gulpTs(compileSettingsForFeature))
 				.js.pipe(gulp.dest(JS_OUTPUT_PATH)));
 }
+
+// すべてのLESSをコンパイルする関数
+function compileAllLess() {
+	
+	del.sync(CSS_OUTPUT_PATH + '/**/*');
+	
+	return gulp.src(LESS_FILES)
+		.pipe(plumber({
+			'errorHandler': function(error) {
+				console.log(error);
+				this.emit('end');
+			}
+		}))
+        .pipe(gulpLess())
+        .pipe(gulp.dest(CSS_OUTPUT_PATH));
+}
+
